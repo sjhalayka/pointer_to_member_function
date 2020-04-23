2b0d34f29aebc42994186929c42bbba57342a0b1
Shawn Halayka <sjhalayka@gmail.com>
2020-04-23 11:38:10 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,25 +2,27 @@
 using namespace std;
 
 class c;
-typedef int (c::* c_member_function_pointer)(size_t x);
+typedef int (c::* c_member_function_pointer)(void);
 
 class c
 {
 public:
 	void proceed(void)
 	{
-		(this->*(this->ptr))(12345);
+		if(ptr != 0)
+			(this->*(this->ptr))();
 	}
 
 protected:
-	int f(size_t x = 1)
+	int f(void)
 	{
-		cout << x << endl;
+		cout << batch_size << endl;
 		return 1;
 	}
 
 	c_member_function_pointer ptr = &c::f;
-	bool b;
+	bool b = true;
+	size_t batch_size = 10;
 };
 
 int main(void)
